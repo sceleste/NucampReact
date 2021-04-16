4a8567f9f0f347cd612779e110027e76e8c6bd90
Serena Celeste <serenaceleste@gmail.com>
2021-04-16 18:59:02 -0400

    Adding to the Single Page App

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,36 +1,31 @@
 import React from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-function RenderDirectoryItem({campsite}) {
+function RenderDirectoryItem({ campsite }) {
   return (
     <Card>
       <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-        <CardImgOverlay>
-          <CardTitle>{campsite.name}</CardTitle>
-        </CardImgOverlay>
+      <CardImgOverlay>
+        <CardTitle>{campsite.name}</CardTitle>
+      </CardImgOverlay>
     </Card>
   );
 }
 
 function Directory(props) {
-    const directory = props.campsites.map((campsite) => {
-      return (
-        <div key={campsite.id} className="col-md-5 m-1">
-          <RenderDirectoryItem campsite={campsite} />
-        </div>
-      );
-    });
-
+  const directory = props.campsites.map((campsite) => {
     return (
-      <div className="container">
-        <div className="row">{directory}</div>
+      <div key={campsite.id} className="col-md-5 m-1">
+        <RenderDirectoryItem campsite={campsite} />
       </div>
     );
+  });
+
+  return (
+    <div className="container">
+      <div className="row">{directory}</div>
+    </div>
+  );
 }
 
 export default Directory;
